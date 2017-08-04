54e50ed31d6e1736d48bdc447cab9aff9569ace9
Bastien RIVIERE <briviere@e2r11p18.42.fr>
2017-08-04 22:49:43 +0200

    Add main for testing

--- sastantua.c
+++ sastantua.c
@@ -6,14 +6,35 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/04 22:38:53 by briviere          #+#    #+#             */
-/*   Updated: 2017/08/04 22:39:27 by briviere         ###   ########.fr       */
+/*   Updated: 2017/08/04 22:49:35 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// Only for debug
+#include <stdio.h>
 #include <unistd.h>
 
+// Only for debug
 int		ft_putchar(char c)
 {
 	write(1, &c, 1);
 	return (0);
+}
+
+void	sastantua(int size)
+{
+	
+}
+
+// Only for debug
+int		main(int argc, char **argv)
+{
+	if (argc <= 1)
+	{
+		printf("Usage: ./sastantua <size>\n");
+		return (1);
+	}
+	
+	int size = atoi(argv[1]);
+	sastantua(size);
 }
