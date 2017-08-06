966b05168f04b28b0d92a1c879c7fc3b0dfd17f6
Bastien RIVIERE <briviere@e2r11p18.42.fr>
2017-08-06 20:06:55 +0200

    Work

--- sastantua.c
+++ sastantua.c
@@ -6,7 +6,7 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/04 22:38:53 by briviere          #+#    #+#             */
-/*   Updated: 2017/08/06 18:09:05 by smokhtar         ###   ########.fr       */
+/*   Updated: 2017/08/06 20:07:01 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,25 +24,42 @@ int		ft_putchar(char c)
 
 void	ft_print_multiple(char c, int size)
 {
-	while (size--)
+	int count;
+
+	count = 0;
+	while (count++ < size)
 		ft_putchar(c);
 }
 
-void	ft_print_last_floor(int floor, int size, int *cur_len, int total)
+
+void	ft_print_last_floor(int floor, int *cur_len, int total_len)
 {
 	int taille;
 	int idx;
 
-	
-	idx = 0;
-	taille = size / 2 - 1;
-	printf("%d", taille);
-	while(0)
+	idx = 1;
+	taille = (floor + (floor % 2)) * 2 / 2 - 1;
+	while (idx <= floor)
 	{
-		ft_print_multiple(' ', (total - *cur_len) / 4);
+		ft_print_multiple(' ', (total_len - *cur_len) / 2);
 		ft_putchar('/');
-		ft_print_multiple('*', (*cur_len - 2) / 2);
-		ft_print_multiple('|', 1 + ( size / 2));
+		ft_print_multiple('*', (*cur_len - 2 - taille) / 2);
+		if (idx == 1 && floor % 2 == 0)
+			ft_print_multiple('*', taille);
+		else
+		{
+			ft_print_multiple('|', taille - 2);
+			if (idx == (floor / 2 + 1) && floor >= 5)
+			{
+				ft_putchar('$');
+				ft_putchar('|');
+			}
+			else if (floor <= 2)
+				ft_putchar('|');
+			else if (taille > 1)
+				ft_print_multiple('|', 2);
+		}
+		ft_print_multiple('*', (*cur_len - 2 - taille) / 2);
 		ft_putchar('\\');
 		ft_putchar('\n');
 		idx += 1;
@@ -71,17 +88,15 @@ int		ft_len_base(int size)
 
 void	ft_print_floor(int floor, int total_floor, int *cur_len, int total_len)
 {
-    int size;
-    int idx;
+	int size;
+	int idx;
 
-    size = floor + 2;
-    idx = 1;
-    if (floor == total_floor)
-	{
-		size = 3 - ( total_floor % 2);
-	}
+	size = floor + 2;
+	idx = 1;
+	if (floor == total_floor)
+		size = 2;
 	while (idx <= size)
-    {
+	{
 		ft_print_multiple(' ', (total_len - *cur_len) / 2);
 		ft_putchar('/');
 		ft_print_multiple('*', *cur_len - 2);
@@ -103,11 +118,12 @@ void	sastantua(int size)
 	floor = 1;
 	while (floor <= size)
 	{
-	    ft_print_floor(floor, size, &cur_len, total_len);
-	    floor++;
-	    cur_len += 4 + 2 * ((floor - 2) / 2);
+		ft_print_floor(floor, size, &cur_len, total_len);
+		if (floor < size)
+			cur_len += 4 + 2 * ((floor - 2) / 2);
+		floor++;
 	}
-	ft_print_last_floor(floor,size, &cur_len, total_len);
+	ft_print_last_floor(floor - 1, &cur_len, total_len);
 }
 
 // Only for debug
